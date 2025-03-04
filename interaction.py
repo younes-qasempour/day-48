8dd9ca9727ed27db10d8bf9b7371696e4cb9895b
younes_qasempour <qasempouryounes@gmail.com>
2025-03-04 11:29:19 +0330

    Working

---
+++ interaction.py
@@ -0,0 +1,16 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_experimental_option("detach", True)
+
+driver = webdriver.Chrome(options=chrome_options)
+driver.get("https://en.wikipedia.org/wiki/Main_Page")
+
+article_count = driver.find_elements(By.CSS_SELECTOR, "#articlecount li a")
+print(article_count[1].text)
+
+
+
+
+driver.quit()
