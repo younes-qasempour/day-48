971bf7fecf396c8da5b003da4bd63a8066a09a70
younes_qasempour <qasempouryounes@gmail.com>
2025-03-04 10:25:02 +0330

    Working

--- main.py
+++ main.py
@@ -12,8 +12,15 @@ driver.get("https://www.python.org")
 # price_cents = driver.find_element(By.CLASS_NAME, value="a-price-fraction")
 # print(f"The price is ${price_dollar.text}.{price_cents.text}")
 
-search_bar = driver.find_element(By.NAME, "q")
-print(search_bar)
+# search_bar = driver.find_element(By.NAME, "q")
+# print(search_bar.get_attribute("placeholder"))
+# button = driver.find_element(By.ID, "submit")
+# print(button.size)
+# documentation_link = driver.find_element(By.CSS_SELECTOR, ".documentation-widget a")
+# print(documentation_link.text)
+# bug_link = driver.find_element(By.XPATH, '//*[@id="site-map"]/div[2]/div/ul/li[3]/a')
+# print(bug_link.get_attribute('href'))
+
 
 # close: Closes a tab
 # driver.close()
