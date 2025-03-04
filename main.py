1060afd837921baf2a5f4a592d8c9d9f6b255382
younes_qasempour <qasempouryounes@gmail.com>
2025-03-04 09:52:59 +0330

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+from selenium import webdriver
+
+# Keep Chrome browser open after program finishes
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_experimental_option("detach", True)
+
+driver = webdriver.Chrome(options=chrome_options)
+driver.get("https://www.amazon.com")
+
+
+
+
+
+# close: Closes a tab
+# driver.close()
+# quit: quit the entire browser
+# driver.quit()
