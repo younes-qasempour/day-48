4b70ca19c57621e094e965445043c3e7d48fd745
younes_qasempour <qasempouryounes@gmail.com>
2025-03-04 11:16:48 +0330

    Working

--- main.py
+++ main.py
@@ -21,6 +21,35 @@ driver.get("https://www.python.org")
 # bug_link = driver.find_element(By.XPATH, '//*[@id="site-map"]/div[2]/div/ul/li[3]/a')
 # print(bug_link.get_attribute('href'))
 
+# My solution
+x = driver.find_element(By.CSS_SELECTOR, ".event-widget div")
+y = x.text.split("\n")[2:]
+k = y[::2]
+j = y[1::2]
+t = {}
+for i in range(len(k)):
+    t.update({i:{"time": k[i], "name":j[i]}})
+# print(t)
+
+# Angela's solution
+event_times = driver.find_elements(By.CSS_SELECTOR, ".event-widget time")
+# for time in event_times:
+#     print(time.text)
+event_names = driver.find_elements(By.CSS_SELECTOR, ".event-widget li a")
+# for name in event_names:
+#     print(name.text)
+events = {}
+for n in range(len(event_times)):
+    events[n] = {
+        "time": event_times[n].text,
+        "name": event_names[n].text,
+    }
+print(events)
+
+
+
+
+
 
 # close: Closes a tab
 # driver.close()
